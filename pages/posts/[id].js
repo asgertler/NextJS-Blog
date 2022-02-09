57adc75d001a99aa576384631a18b38b026e0c00
Aaron Gertler <aaron.gertler@gmail.com>
2022-02-09 15:56:49 -0500

    refactored homepage to include routing

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,6 +2,7 @@ import Layout from '../../components/Layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
@@ -23,17 +24,19 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
     return (
         <Layout>
-            <Head>
-                <title>{postData.title}</title>
-            </Head>
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
 
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+
+          <div className={utilStyles.lightText}>
             <Date dateString={postData.date} />
-            <br />
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </Layout>
+          </div>
+          
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+      </Layout>
     )
 }
