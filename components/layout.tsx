e4cc7c5a2b3ab40f421f95671aeccbc67b4783d5
Aaron Gertler <aaron.gertler@gmail.com>
2022-02-09 17:22:33 -0500

    converted app from react js to react tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,13 @@ import Link from 'next/link'
 const name = 'Aaron'
 export const siteTitle = "Aaron' Next.js Blog"
 
-export default function Layout({ children, home }) {
+export default function Layout({ 
+  children, 
+  home
+ }: {
+   children: React.ReactNode
+   home?: boolean
+ }) {
   return (
     <div className={styles.container}>
       <Head>
